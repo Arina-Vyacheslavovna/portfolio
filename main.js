631950765070c4f9cc47a68d2e788e170089aeff
Arina <ari3488@yandex.ru>
2020-12-10 13:08:18 +0300

    fifth commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// // var ham = document.getElementById("ham");
+// // ham.onclick = function() {
+// //     ham.classList.toggle("clicked");
+// // }
+
+// // document.querySelector(".burger").addEventListener("click", function () {
+// //     document.querySelector(".burger span").classList.toggle("active");
+// //     document.querySelector("ul").classList.toggle("animate");
+// //   });
+
+//   document.querySelector(".burger").addEventListener("click", function () {
+//     document.querySelector(".burger span").classList.toggle("active");
+//     document.querySelector(".nav").classList.toggle("animate");
+//   });
